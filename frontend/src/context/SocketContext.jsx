f2bef497b6ba8ba1d086a6e8fae4b31302149197
Abhay Pandey <abhaypandey.1144@gmail.com>
2026-03-28 05:03:01 +0530

    key change

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -7,7 +7,11 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:5000');
+    const socketURL = import.meta.env.VITE_API_URL 
+      ? import.meta.env.VITE_API_URL.replace('/api', '') 
+      : 'http://localhost:5000';
+      
+    const newSocket = io(socketURL);
     setSocket(newSocket);
 
     return () => newSocket.close();
